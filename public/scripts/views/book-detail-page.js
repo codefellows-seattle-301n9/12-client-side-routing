ce554e786c780cb6036ee4f600f175637c4f5a06
ayanle <ayanle.abdi@gmail.com>
2018-01-13 16:08:17 -0800

    slight touch up of css

--- public/scripts/views/book-detail-page.js
+++ public/scripts/views/book-detail-page.js
@@ -4,12 +4,12 @@ var app = app || {};
     const bookDetailPage = {};
 
     const markup = `
-            <div class="col-md-12">
-                <div class="row">
-                    <div class="col-md-6">
+            <div>
+                <div>
+                    <div>
                         <img src="{{{image_url}}}" alt="" width="400">
                     </div>
-                    <div class="col-md-6">
+                    <div>
                         <h3>{{title}}</h3>
                         <h4>by {{author}}</h4>
                         
